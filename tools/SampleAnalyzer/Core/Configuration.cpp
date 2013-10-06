7176c31d86e9982975f22c99663e22f4f9ab2870
Benjamin Fuks <fuks@cern.ch>
2013-10-06 17:56:11 +0200

    compatibility with mac improved

--- tools/SampleAnalyzer/Core/Configuration.cpp
+++ tools/SampleAnalyzer/Core/Configuration.cpp
@@ -39,8 +39,8 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.1.10";
-const std::string Configuration::sampleanalyzer_date_    = "2013/09/12";
+const std::string Configuration::sampleanalyzer_version_ = "1.1.11";
+const std::string Configuration::sampleanalyzer_date_    = "2013/10/06";
 // DO NOT TOUCH THESE LINES
 
 // -----------------------------------------------------------------------------
