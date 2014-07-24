3fcdc51d5670a09909e4d0b92a1c1c12ee2e797e
Benjamin Fuks <fuks@cern.ch>
2014-07-24 17:25:48 +0100

    version number fix

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -39,8 +39,8 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.1.11";
-const std::string Configuration::sampleanalyzer_date_    = "2014/07/17";
+const std::string Configuration::sampleanalyzer_version_ = "1.1.12";
+const std::string Configuration::sampleanalyzer_date_    = "2014/07/24";
 // DO NOT TOUCH THESE LINES
 
 // -----------------------------------------------------------------------------
