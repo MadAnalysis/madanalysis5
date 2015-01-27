dcc1bb5e768f44ce53aa64a9ebf7acd2f4365a90
Benjamin Fuks <fuks@cern.ch>
2015-01-26 23:01:47 -0600

    Compilation errors when one core was selected fixed

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -39,8 +39,8 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.1.12.4";
-const std::string Configuration::sampleanalyzer_date_    = "2014/11/11";
+const std::string Configuration::sampleanalyzer_version_ = "1.1.12.5";
+const std::string Configuration::sampleanalyzer_date_    = "2015/01/26";
 // DO NOT TOUCH THESE LINES
 
 // -----------------------------------------------------------------------------
