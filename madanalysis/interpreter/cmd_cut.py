77b4893400c5da9fd8d53b2d6909660298ee371f
Benjamin Fuks <fuks@cern.ch>
2015-09-28 21:18:28 +0200

    Bug fix in the cut condition decoder

--- madanalysis/interpreter/cmd_cut.py
+++ madanalysis/interpreter/cmd_cut.py
@@ -157,11 +157,19 @@ class CmdCut(CmdBase,CmdSelectionBase):
         if len(sequence)<2:
             return sequence
         if sequence[0]=='(' and sequence[-1]==')':
+            nopened=0
+            for i in range(len(sequence)):
+                if sequence[i]=="(":
+                    nopened+=1
+                elif sequence[i]==")":
+                    nopened-=1
+                if nopened==0 and i!=0 and i!=len(sequence)-1:
+                    return sequence
             return self.clean_sequence(sequence[1:-1])
         else:
             return sequence
 
-    
+
     def extract_sequence(self,current,sequence,partType):
 
         # Remove extra braces
