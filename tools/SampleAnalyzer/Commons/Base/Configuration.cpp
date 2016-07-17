0493a895535800d7dfd5dada8a9c9393dea943e9
Benjamin Fuks <fuks@cern.ch>
2016-07-17 10:48:29 +0200

    install pad fixed

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -39,8 +39,8 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.4.20";
-const std::string Configuration::sampleanalyzer_date_    = "2016/07/15";
+const std::string Configuration::sampleanalyzer_version_ = "1.4.21";
+const std::string Configuration::sampleanalyzer_date_    = "2016/07/17";
 // DO NOT TOUCH THESE LINES
 
 // -----------------------------------------------------------------------------
