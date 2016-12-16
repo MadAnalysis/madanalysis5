ce2cca4d6e20dff89b103ae6a26bc620c1f8d8cd
Eric Conte <eric.conte@cern.ch>
2016-12-16 11:39:17 +0100

    change release note

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -40,8 +40,8 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.5.25";
-const std::string Configuration::sampleanalyzer_date_    = "2016/11/14";
+const std::string Configuration::sampleanalyzer_version_ = "1.6";
+const std::string Configuration::sampleanalyzer_date_    = "2016/12/16";
 // DO NOT TOUCH THESE LINES
 
 // -----------------------------------------------------------------------------
