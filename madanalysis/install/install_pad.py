715d09dda2d92b49878ad608411a603746c67601
Eric Conte <eric.conte@cern.ch>
2016-12-28 15:39:07 +0100

    fix bug with install PAD

--- madanalysis/install/install_pad.py
+++ madanalysis/install/install_pad.py
@@ -222,6 +222,7 @@ class InstallPad:
 
         # Initialize the expert mode
         logging.getLogger('MA5').debug('Calling the expert mode for file CMS_B2G_12_012')
+        logging.getLogger('MA5').debug('BEGIN ExpertMode')
         from madanalysis.core.expert_mode import ExpertMode
         expert = ExpertMode(self.main)
         dirname="PAD"
@@ -230,6 +231,7 @@ class InstallPad:
         filename="CMS_B2G_12_012"
         if not expert.Copy(dirname):
             return False
+        logging.getLogger('MA5').debug('END ExpertMode')
 
 #        TheCommand = ['bin/ma5', '-R', '-E', '-f', 'PAD', 'CMS_B2G_12_012']
 #        logging.getLogger('MA5').debug(' '.join(TheCommand))
--- madanalysis/layout/plotflow.py
+++ madanalysis/layout/plotflow.py
@@ -437,13 +437,11 @@ class PlotFlow:
         axis_titleY = ref.GetYaxis()
 
         # Scale to one ?
-        print "ERIC"
         scale2one = False
         if ref.stack==StackingMethodType.NORMALIZE2ONE or \
            (self.main.stack==StackingMethodType.NORMALIZE2ONE and \
            ref.stack==StackingMethodType.AUTO):
             scale2one = True
-        print "ERIC: scale2one" + str(scale2one)
 
         if scale2one:
             axis_titleY += " ( scaled to one )"
