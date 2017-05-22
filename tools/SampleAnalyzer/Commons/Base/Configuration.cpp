1f5aed380dac11eb7f74af6fd224bc6fc7f116b9
Benjamin Fuks <fuks@cern.ch>
2017-05-22 22:53:58 +0100

    zlib detection triggering an unwanted compilation within the mg5 ...

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -40,7 +40,7 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.6.19";
+const std::string Configuration::sampleanalyzer_version_ = "1.6.20";
 const std::string Configuration::sampleanalyzer_date_    = "2017/05/22";
 // DO NOT TOUCH THESE LINES
 
