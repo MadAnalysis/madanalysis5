65da12edcf83ca8ece8af5c82f864cbf9cec153a
Benjamin Fuks <fuks@cern.ch>
2017-08-23 11:43:00 +0900

    Adding weights to the analysis template in the NewAnalyzer script

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -40,7 +40,7 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.6.28";
+const std::string Configuration::sampleanalyzer_version_ = "1.6.29";
 const std::string Configuration::sampleanalyzer_date_    = "2017/08/23";
 // DO NOT TOUCH THESE LINES
 
