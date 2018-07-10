4caec3f6cd2860bed9794f2c65d25dd29c9e1167
Benjamin Fuks <fuks@cern.ch>
2018-07-10 12:33:36 +0200

    Bug fix #1780945

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -40,7 +40,7 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.7.5";
+const std::string Configuration::sampleanalyzer_version_ = "1.7.6";
 const std::string Configuration::sampleanalyzer_date_    = "2018/07/10";
 // DO NOT TOUCH THESE LINES
 
