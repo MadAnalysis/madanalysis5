bcabd3dcadc7b2e5b6d71de1dc8e6dc8a347e790
Eric Conte <eric.conte@cern.ch>
2018-11-21 04:46:10 +0100

    syntax error in fastjet install

--- madanalysis/install/install_fastjet.py
+++ madanalysis/install/install_fastjet.py
@@ -43,7 +43,7 @@ class InstallFastjet:
 #        self.files = {"fastjet.tar.gz" : "http://madanalysis.irmp.ucl.ac.be/raw-attachment/wiki/WikiStart/fastjet-3.2.0.tar.gz"}
 #        self.files = {"fastjet.tar.gz" : "http://madanalysis.irmp.ucl.ac.be/raw-attachment/wiki/WikiStart/fastjet-3.2.1.tar.gz"}
 #        self.files = {"fastjet.tar.gz" : "http://madanalysis.irmp.ucl.ac.be/raw-attachment/wiki/WikiStart/fastjet-3.3.0.tar.gz"}
-         self.files = {"fastjet.tar.gz" : "http://madanalysis.irmp.ucl.ac.be/raw-attachment/wiki/WikiStart/fastjet-3.3.2.tar.gz"}
+        self.files = {"fastjet.tar.gz" : "http://madanalysis.irmp.ucl.ac.be/raw-attachment/wiki/WikiStart/fastjet-3.3.2.tar.gz"}
 
     def Detect(self):
         if not os.path.isdir(self.toolsdir):
