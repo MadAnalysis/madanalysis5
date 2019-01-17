d66d597ffccdc3c0b4bd034fb4b7459f8245cffe
Benjamin Fuks <fuks@cern.ch>
2019-01-17 09:16:46 +0100

    Fixing the linecolor bug #1807107 (connected to matplotlib)

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -40,7 +40,7 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.7.18";
+const std::string Configuration::sampleanalyzer_version_ = "1.7.19";
 const std::string Configuration::sampleanalyzer_date_    = "2019/01/17";
 // DO NOT TOUCH THESE LINES
 
