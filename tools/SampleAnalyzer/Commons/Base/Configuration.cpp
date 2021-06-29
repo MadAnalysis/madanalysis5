1aa017ce8e50d8578d3082687b28e8e168d5d506
fuks@cern.ch <>
2021-06-29 10:45:05 +0200

    Allowing for python 3.6

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -40,8 +40,8 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.9.24";
-const std::string Configuration::sampleanalyzer_date_    = "2021/06/15";
+const std::string Configuration::sampleanalyzer_version_ = "1.9.25";
+const std::string Configuration::sampleanalyzer_date_    = "2021/06/29";
 // DO NOT TOUCH THESE LINES
 
 // -----------------------------------------------------------------------------
