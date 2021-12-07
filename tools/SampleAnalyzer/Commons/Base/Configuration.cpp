da448706f94c24c2952181e5424fd923d1e0bb10
fuks@cern.ch <>
2021-12-07 17:17:04 +0100

    Small fix for analyses carrying a too long filename

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -40,8 +40,8 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.9.58";
-const std::string Configuration::sampleanalyzer_date_    = "2021/12/06";
+const std::string Configuration::sampleanalyzer_version_ = "1.9.59";
+const std::string Configuration::sampleanalyzer_date_    = "2021/12/07";
 // DO NOT TOUCH THESE LINES
 
 // -----------------------------------------------------------------------------
