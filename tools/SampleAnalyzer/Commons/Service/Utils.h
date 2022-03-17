ad43e9e83609f5bbe24c405dfb9229a3ace654c3
jackaraz <jackaraz@gmail.com>
2022-03-17 17:59:18 +0000

    add vector operations

--- tools/SampleAnalyzer/Commons/Service/Utils.h
+++ tools/SampleAnalyzer/Commons/Service/Utils.h
@@ -42,6 +42,28 @@
 namespace MA5
 {
 
+    // ============================= //
+    // ===== Vector operations ===== //
+    // ============================= //
+
+    template <typename T>
+    std::vector<T> operator+(const std::vector<T> &A, const std::vector<T> &B)
+    {
+        std::vector<T> AB;
+        AB.reserve(A.size() + B.size());          // preallocate memory
+        AB.insert(AB.end(), A.begin(), A.end());  // add A;
+        AB.insert(AB.end(), B.begin(), B.end());  // add B;
+        return AB;
+    }
+
+    template <typename T>
+    std::vector<T> &operator+=(std::vector<T> &A, const std::vector<T> &B)
+    {
+        A.reserve(A.size() + B.size());         // preallocate memory without erase original data
+        A.insert(A.end(), B.begin(), B.end());  // add B;
+        return A;                               // here A could be named AB
+    }
+
     //===========//
     // Filtering //
     //===========//
