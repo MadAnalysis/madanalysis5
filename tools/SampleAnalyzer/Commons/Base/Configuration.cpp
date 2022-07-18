d8b82f7f563cbbd2325055bd5d2d2525be7285a6
jackaraz <jackaraz@gmail.com>
2022-07-18 09:55:53 +0100

    bump version minor

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -40,8 +40,8 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "2.0.1";
-const std::string Configuration::sampleanalyzer_date_    = "2022/07/04";
+const std::string Configuration::sampleanalyzer_version_ = "2.0.2";
+const std::string Configuration::sampleanalyzer_date_    = "2022/07/18";
 // DO NOT TOUCH THESE LINES
 
 // -----------------------------------------------------------------------------
