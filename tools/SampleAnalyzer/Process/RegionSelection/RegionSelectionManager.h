20a2e6c3c9e5426765c9c749e1e6113136bfffce
kfan326 <kfan326@gmail.com>
2022-07-23 10:41:51 -0600

    fixed regionSelectionManager destructor
    destructor now deletes allocated region selection objects when going out of scope.

--- tools/SampleAnalyzer/Process/RegionSelection/RegionSelectionManager.h
+++ tools/SampleAnalyzer/Process/RegionSelection/RegionSelectionManager.h
@@ -73,7 +73,12 @@ class RegionSelectionManager
   RegionSelectionManager() {};
 
   /// Destructor
-  ~RegionSelectionManager() { };
+  // 
+  ~RegionSelectionManager() { 
+    for(auto &region_pointer : regions_){
+      delete region_pointer;
+    }
+  };
 
   /// Reset
   void Reset()
