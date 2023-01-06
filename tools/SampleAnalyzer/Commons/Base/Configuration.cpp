4f603713f1e4ece3b9d7c4a3314ef576b44c3194
BFuks <fuks@lpthe.jussieu.fr>
2023-01-06 16:56:23 +0100

    Conflicting version numbers

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -40,7 +40,7 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.10.6";
+const std::string Configuration::sampleanalyzer_version_ = "1.10.7";
 const std::string Configuration::sampleanalyzer_date_    = "2023/01/06";
 // DO NOT TOUCH THESE LINES
 
