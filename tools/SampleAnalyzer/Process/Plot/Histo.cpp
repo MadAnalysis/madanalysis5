bfab5b20b5cb99bc19b43c73dc88c1d499c5d41c
Jack Y. Araz <jackaraz@gmail.com>
2023-06-24 12:05:11 +0100

    multiweight adaptation

--- tools/SampleAnalyzer/Process/Plot/Histo.cpp
+++ tools/SampleAnalyzer/Process/Plot/Histo.cpp
@@ -1,35 +1,33 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  
+//
 //  Copyright (C) 2012-2023 Jack Araz, Eric Conte & Benjamin Fuks
 //  The MadAnalysis development team, email: <ma5team@iphc.cnrs.fr>
-//  
+//
 //  This file is part of MadAnalysis 5.
 //  Official website: <https://github.com/MadAnalysis/madanalysis5>
-//  
+//
 //  MadAnalysis 5 is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  MadAnalysis 5 is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with MadAnalysis 5. If not, see <http://www.gnu.org/licenses/>
-//  
+//
 ////////////////////////////////////////////////////////////////////////////////
-
 
 // SampleAnalyzer headers
 #include "SampleAnalyzer/Process/Plot/Histo.h"
 
 using namespace MA5;
 
-
 /// Write the plot in a Text file
-void Histo::Write_TextFormat(std::ostream* output)
+void Histo::Write_TextFormat(std::ostream *output)
 {
   // Header
   *output << "<Histo>" << std::endl;
@@ -42,9 +40,8 @@ void Histo::Write_TextFormat(std::ostream* output)
   *output << std::endl;
 }
 
-
 /// Write the plot in a Text file
-void Histo::Write_TextFormatBody(std::ostream* output)
+void Histo::Write_TextFormatBody(std::ostream *output)
 {
   // Description
   *output << "  <Description>" << std::endl;
@@ -52,7 +49,7 @@ void Histo::Write_TextFormatBody(std::ostream* output)
   // Name
   *output << "    \"" << name_ << "\"" << std::endl;
 
-  // Title 
+  // Title
   *output << "    ";
   output->width(10);
   *output << std::left << "# nbins";
@@ -63,21 +60,25 @@ void Histo::Write_TextFormatBody(std::ostream* output)
 
   // Data
   *output << "      ";
-  output->width( 8); *output << std::left << nbins_;
-  output->width(15); *output << std::left << std::scientific << xmin_;
-  output->width(15); *output << std::left << std::scientific << xmax_ << std::endl;
+  output->width(8);
+  *output << std::left << nbins_;
+  output->width(15);
+  *output << std::left << std::scientific << xmin_;
+  output->width(15);
+  *output << std::left << std::scientific << xmax_ << std::endl;
 
   // SelectionRegions
-  if(regions_.size()!=0)
+  if (regions_.size() != 0)
   {
-    MAuint32 maxlength=0;
-    for(MAuint32 i=0; i < regions_.size(); i++)
-      if (regions_[i]->GetName().size()>maxlength) maxlength=regions_[i]->GetName().size();
+    MAuint32 maxlength = 0;
+    for (MAuint32 i = 0; i < regions_.size(); i++)
+      if (regions_[i]->GetName().size() > maxlength)
+        maxlength = regions_[i]->GetName().size();
     *output << std::left << "    # Defined regions" << std::endl;
-    for(MAuint32 i=0; i < regions_.size(); i++)
+    for (MAuint32 i = 0; i < regions_.size(); i++)
     {
       *output << "      " << std::setw(maxlength) << std::left << regions_[i]->GetName();
-      *output << "    # Region nr. " << std::fixed << i+1 << std::endl;
+      *output << "    # Region nr. " << std::fixed << i + 1 << std::endl;
     }
   }
 
@@ -88,54 +89,73 @@ void Histo::Write_TextFormatBody(std::ostream* output)
   *output << "  <Statistics>" << std::endl;
 
   *output << "      ";
-  output->width(15); *output << std::fixed << nevents_.first;
-  output->width(15); *output << std::fixed << nevents_.second;
+  output->width(15);
+  *output << std::fixed << nevents_.first;
+  output->width(15);
+  *output << std::fixed << nevents_.second;
   *output << " # nevents" << std::endl;
   *output << "      ";
-  output->width(15); *output << std::scientific << nevents_w_.first;
-  output->width(15); *output << std::scientific << nevents_w_.second;
+  output->width(15);
+  *output << std::scientific << nevents_w_.first;
+  output->width(15);
+  *output << std::scientific << nevents_w_.second;
   *output << " # sum of event-weights over events" << std::endl;
   *output << "      ";
-  output->width(15); *output << std::fixed << nentries_.first;
-  output->width(15); *output << std::fixed << nentries_.second;
+  output->width(15);
+  *output << std::fixed << nentries_.first;
+  output->width(15);
+  *output << std::fixed << nentries_.second;
   *output << " # nentries" << std::endl;
   *output << "      ";
-  output->width(15); *output << std::scientific << sum_w_.first;
-  output->width(15); *output << std::scientific << sum_w_.second;
+  output->width(15);
+  *output << std::scientific << sum_w_[0].first;
+  output->width(15);
+  *output << std::scientific << sum_w_[0].second;
   *output << " # sum of event-weights over entries" << std::endl;
   *output << "      ";
-  output->width(15); *output << std::scientific << sum_ww_.first;
-  output->width(15); *output << std::scientific << sum_ww_.second;
+  output->width(15);
+  *output << std::scientific << sum_ww_[0].first;
+  output->width(15);
+  *output << std::scientific << sum_ww_[0].second;
   *output << " # sum weights^2" << std::endl;
   *output << "      ";
-  output->width(15); *output << std::scientific << sum_xw_.first;
-  output->width(15); *output << std::scientific << sum_xw_.second;
+  output->width(15);
+  *output << std::scientific << sum_xw_[0].first;
+  output->width(15);
+  *output << std::scientific << sum_xw_[0].second;
   *output << " # sum value*weight" << std::endl;
   *output << "      ";
-  output->width(15); *output << std::scientific << sum_xxw_.first;
-  output->width(15); *output << std::scientific << sum_xxw_.second;
+  output->width(15);
+  *output << std::scientific << sum_xxw_[0].first;
+  output->width(15);
+  *output << std::scientific << sum_xxw_[0].second;
   *output << " # sum value^2*weight" << std::endl;
   *output << "  </Statistics>" << std::endl;
 
   // Data
   *output << "  <Data>" << std::endl;
   *output << "      ";
-  output->width(15); *output << std::scientific << underflow_.first;
-  output->width(15); *output << std::scientific << underflow_.second;
+  output->width(15);
+  *output << std::scientific << underflow_[0].first;
+  output->width(15);
+  *output << std::scientific << underflow_[0].second;
   *output << " # underflow" << std::endl;
-  for (MAuint32 i=0;i<histo_.size();i++)
+  for (MAuint32 i = 0; i < histo_.size(); i++)
   {
     *output << "      ";
-    output->width(15); *output << std::scientific << histo_[i].first;
-    output->width(15); *output << std::scientific << histo_[i].second;
-    if (i<2 || i>=(histo_.size()-2))
-      *output << " # bin " << i+1 << " / " << histo_.size();
+    output->width(15);
+    *output << std::scientific << histo_[0][i].first;
+    output->width(15);
+    *output << std::scientific << histo_[0][i].second;
+    if (i < 2 || i >= (histo_.size() - 2))
+      *output << " # bin " << i + 1 << " / " << histo_.size();
     *output << std::endl;
   }
   *output << "      ";
-  output->width(15); *output << std::scientific << overflow_.first;
-  output->width(15); *output << std::scientific << overflow_.second;
+  output->width(15);
+  *output << std::scientific << overflow_[0].first;
+  output->width(15);
+  *output << std::scientific << overflow_[0].second;
   *output << " # overflow" << std::endl;
   *output << "  </Data>" << std::endl;
 }
-
--- tools/SampleAnalyzer/Process/Plot/Histo.h
+++ tools/SampleAnalyzer/Process/Plot/Histo.h
@@ -1,30 +1,28 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  
+//
 //  Copyright (C) 2012-2023 Jack Araz, Eric Conte & Benjamin Fuks
 //  The MadAnalysis development team, email: <ma5team@iphc.cnrs.fr>
-//  
+//
 //  This file is part of MadAnalysis 5.
 //  Official website: <https://github.com/MadAnalysis/madanalysis5>
-//  
+//
 //  MadAnalysis 5 is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  MadAnalysis 5 is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with MadAnalysis 5. If not, see <http://www.gnu.org/licenses/>
-//  
+//
 ////////////////////////////////////////////////////////////////////////////////
-
 
 #ifndef HISTO_H
 #define HISTO_H
-
 
 // STL headers
 #include <map>
@@ -36,186 +34,199 @@
 #include "SampleAnalyzer/Process/RegionSelection/RegionSelection.h"
 #include "SampleAnalyzer/Commons/Service/ExceptionService.h"
 
-
 namespace MA5
 {
 
-class Histo : public PlotBase
-{
-
-  // -------------------------------------------------------------
-  //                        data members
-  // -------------------------------------------------------------
- protected :
-
-  /// Histogram arrays
-  std::vector< std::pair<MAfloat64,MAfloat64> > histo_;
-  std::pair<MAfloat64, MAfloat64> underflow_;
-  std::pair<MAfloat64, MAfloat64> overflow_;
-
-  /// Histogram description
-  MAuint32  nbins_;
-  MAfloat64 xmin_;
-  MAfloat64 xmax_;
-  MAfloat64 step_;
-
-  /// Sum of event-weights over entries
-  std::pair<MAfloat64,MAfloat64> sum_w_;
-
-  /// Sum of squared weights
-  std::pair<MAfloat64,MAfloat64> sum_ww_;
-
-  /// Sum of value * weight
-  std::pair<MAfloat64,MAfloat64> sum_xw_;
-
-  /// Sum of value * value * weight
-  std::pair<MAfloat64,MAfloat64> sum_xxw_;
-
-  /// RegionSelections attached to the histo
-  std::vector<RegionSelection*> regions_;
-
-  // -------------------------------------------------------------
-  //                       method members
-  // -------------------------------------------------------------
- public :
-
-  /// Constructor without argument
-  Histo() : PlotBase()
-  {
-    nbins_=100; xmin_=0; xmax_=100;
-    step_ = (xmax_ - xmin_)/static_cast<MAfloat64>(nbins_);
-  }
-
-  /// Constructor with argument 
-  Histo(const std::string& name) : PlotBase(name)
-  { }
-
-  /// Constructor with argument 
-  Histo(const std::string& name, MAuint32 nbins, MAfloat64 xmin, MAfloat64 xmax) :
-     PlotBase(name)
-  { 
-    // Setting the description: nbins
-    try
+    class Histo : public PlotBase
     {
-      if (nbins==0) throw EXCEPTION_WARNING("nbins cannot be equal to 0. Set 100.","",0);
-      nbins_ = nbins;
-    }
-    catch (const std::exception& e)
-    {
-      MANAGE_EXCEPTION(e);
-      nbins_ = 100;
-    }
 
-    // Setting the description: min & max
-    try
-    {
-      if (xmin>=xmax) throw EXCEPTION_WARNING("xmin cannot be equal to or greater than xmax. Setting xmin to 0 and xmax to 100.","",0);
-      xmin_ = xmin;
-      xmax_ = xmax;
-    }
-    catch (const std::exception& e)
-    {
-      MANAGE_EXCEPTION(e);
-      xmin_=0.;
-      xmax_=100.;
-    }    
+        // -------------------------------------------------------------
+        //                        data members
+        // -------------------------------------------------------------
+    protected:
+        /// Histogram arrays
+        std::map<MAint32, std::vector<std::pair<MAfloat64, MAfloat64>>> histo_;
+        std::map<MAint32, std::pair<MAfloat64, MAfloat64>> underflow_;
+        std::map<MAint32, std::pair<MAfloat64, MAfloat64>> overflow_;
 
-    step_ = (xmax_ - xmin_)/static_cast<MAfloat64>(nbins_);
+        /// Histogram description
+        MAuint32 nbins_;
+        MAfloat64 xmin_;
+        MAfloat64 xmax_;
+        MAfloat64 step_;
 
-    // Reseting the histogram array
-    histo_.resize(nbins_,std::make_pair(0.,0.));
-    underflow_ = std::make_pair(0.,0.);
-    overflow_  = std::make_pair(0.,0.);
+        /// Sum of event-weights over entries
+        std::map<MAint32, std::pair<MAfloat64, MAfloat64>> sum_w_;
 
-    // Reseting statistical counters
-    sum_w_    = std::make_pair(0.,0.);
-    sum_ww_   = std::make_pair(0.,0.);
-    sum_xw_   = std::make_pair(0.,0.);
-    sum_xxw_  = std::make_pair(0.,0.);
-  }
+        /// Sum of squared weights
+        std::map<MAint32, std::pair<MAfloat64, MAfloat64>> sum_ww_;
 
-  /// Destructor
-  virtual ~Histo()
-  { }
+        /// Sum of value * weight
+        std::map<MAint32, std::pair<MAfloat64, MAfloat64>> sum_xw_;
 
-  /// Setting the linked regions
-  void SetSelectionRegions(std::vector<RegionSelection*> myregions)
-    { regions_.insert(regions_.end(), myregions.begin(), myregions.end()); }
+        /// Sum of value * value * weight
+        std::map<MAint32, std::pair<MAfloat64, MAfloat64>> sum_xxw_;
 
-  /// Checking that all regions of the histo are surviving
-  /// Returns 0 if all regions are failing (includes te case with 0 SR)
-  /// Returns 1 if all regions are passing 
-  // returns -1 otherwise
-  MAint32 AllSurviving()
-  {
-    if (regions_.size() == 0) return 0;
-    MAbool FirstRegionSurvival = regions_[0]->IsSurviving();
-    for(MAuint32 ii=1; ii < regions_.size(); ii++)
-      if(regions_[ii]->IsSurviving() != FirstRegionSurvival) return -1;
-    if(FirstRegionSurvival) return 1;
-    else                    return 0;
-  }
+        /// RegionSelections attached to the histo
+        std::vector<RegionSelection *> regions_;
 
-  /// Filling histogram
-  void Fill(MAfloat64 value, MAfloat64 weight=1.0)
-  {
-    // Safety : nan or isinf
-    try
-    {
-      if (std::isnan(value)) throw EXCEPTION_WARNING("Skipping a NaN (Not a Number) value in an histogram.","",0); 
-      if (std::isinf(value)) throw EXCEPTION_WARNING("Skipping a Infinity value in an histogram.","",0); 
-    }
-    catch (const std::exception& e)
-    {
-      MANAGE_EXCEPTION(e);
-    }
+        // -------------------------------------------------------------
+        //                       method members
+        // -------------------------------------------------------------
+    public:
+        /// Constructor without argument
+        Histo() : PlotBase()
+        {
+            nbins_ = 100;
+            xmin_ = 0;
+            xmax_ = 100;
+            step_ = (xmax_ - xmin_) / static_cast<MAfloat64>(nbins_);
+        }
 
-    // Positive weight
-    if (weight>=0)
-    {
-      nentries_.first++;
-      sum_w_.first   +=weight;
-      sum_ww_.first  +=weight*weight;
-      sum_xw_.first  +=value*weight;
-      sum_xxw_.first +=value*value*weight;
-      if (value < xmin_) underflow_.first+=weight;
-      else if (value >= xmax_) overflow_.first+=weight;
-      else
-      {
-        histo_[std::floor((value-xmin_)/step_)].first+=weight;
-      }
-    }
+        /// Constructor with argument
+        Histo(const std::string &name) : PlotBase(name) {}
 
-    // Negative weight
-    else
-    {
-      nentries_.second++;
-      weight=std::abs(weight);
-      sum_w_.second   += weight;
-      sum_ww_.second  += weight*weight;
-      sum_xw_.second  += value*weight;
-      sum_xxw_.second += value*value*weight;
-      if (value < xmin_) underflow_.second+=weight;
-      else if (value >= xmax_) overflow_.second+=weight;
-      else
-      {
-        histo_[std::floor((value-xmin_)/step_)].second+=weight;
-      }
-    }
-  }
+        /// Constructor with argument
+        Histo(const std::string &name, MAuint32 nbins, MAfloat64 xmin, MAfloat64 xmax) : PlotBase(name)
+        {
+            // Setting the description: nbins
+            try
+            {
+                if (nbins == 0)
+                    throw EXCEPTION_WARNING("nbins cannot be equal to 0. Set 100.", "", 0);
+                nbins_ = nbins;
+            }
+            catch (const std::exception &e)
+            {
+                MANAGE_EXCEPTION(e);
+                nbins_ = 100;
+            }
 
-  /// Write the plot in a ROOT file
-  virtual void Write_TextFormat(std::ostream* output);
+            // Setting the description: min & max
+            try
+            {
+                if (xmin >= xmax)
+                    throw EXCEPTION_WARNING("xmin cannot be equal to or greater than xmax. Setting xmin to 0 and xmax to 100.", "", 0);
+                xmin_ = xmin;
+                xmax_ = xmax;
+            }
+            catch (const std::exception &e)
+            {
+                MANAGE_EXCEPTION(e);
+                xmin_ = 0.;
+                xmax_ = 100.;
+            }
 
-  /// Write the plot in a ROOT file
-  //  virtual void Write_RootFormat(std::pair<TH1F*,TH1F*>& histos);
+            step_ = (xmax_ - xmin_) / static_cast<MAfloat64>(nbins_);
 
- protected:
+            // Reseting the histogram array
+            histo_[0].resize(nbins_, std::make_pair(0., 0.));
+            underflow_[0] = std::make_pair(0., 0.);
+            overflow_[0] = std::make_pair(0., 0.);
 
-  /// Write the plot in a ROOT file
-  virtual void Write_TextFormatBody(std::ostream* output);
+            // Reseting statistical counters
+            sum_w_[0] = std::make_pair(0., 0.);
+            sum_ww_[0] = std::make_pair(0., 0.);
+            sum_xw_[0] = std::make_pair(0., 0.);
+            sum_xxw_[0] = std::make_pair(0., 0.);
+        }
 
-};
+        /// Destructor
+        virtual ~Histo() {}
+
+        /// Setting the linked regions
+        void SetSelectionRegions(std::vector<RegionSelection *> myregions)
+        {
+            regions_.insert(regions_.end(), myregions.begin(), myregions.end());
+        }
+
+        /// Checking that all regions of the histo are surviving
+        /// Returns 0 if all regions are failing (includes te case with 0 SR)
+        /// Returns 1 if all regions are passing
+        // returns -1 otherwise
+        MAint32 AllSurviving()
+        {
+            if (regions_.size() == 0)
+                return 0;
+            MAbool FirstRegionSurvival = regions_[0]->IsSurviving();
+            for (MAuint32 ii = 1; ii < regions_.size(); ii++)
+                if (regions_[ii]->IsSurviving() != FirstRegionSurvival)
+                    return -1;
+            if (FirstRegionSurvival)
+                return 1;
+            else
+                return 0;
+        }
+
+        /// Filling histogram
+        void Fill(MAfloat64 value, std::map<MAint32, MAdouble64> weights)
+        {
+            // Safety : nan or isinf
+            try
+            {
+                if (std::isnan(value))
+                    throw EXCEPTION_WARNING("Skipping a NaN (Not a Number) value in an histogram.", "", 0);
+                if (std::isinf(value))
+                    throw EXCEPTION_WARNING("Skipping a Infinity value in an histogram.", "", 0);
+            }
+            catch (const std::exception &e)
+            {
+                MANAGE_EXCEPTION(e);
+            }
+
+            for (auto &wmap : weights)
+            {
+                MAdouble64 weight = wmap.second;
+                MAint32 idx = wmap.first;
+                // Positive weight
+                if (weight >= 0)
+                {
+                    nentries_.first++;
+                    sum_w_[idx].first += weight;
+                    sum_ww_[idx].first += weight * weight;
+                    sum_xw_[idx].first += value * weight;
+                    sum_xxw_[idx].first += value * value * weight;
+                    if (value < xmin_)
+                        underflow_[idx].first += weight;
+                    else if (value >= xmax_)
+                        overflow_[idx].first += weight;
+                    else
+                    {
+                        histo_[idx][std::floor((value - xmin_) / step_)].first += weight;
+                    }
+                }
+
+                // Negative weight
+                else
+                {
+                    nentries_.second++;
+                    weight = std::abs(weight);
+                    sum_w_[idx].second += weight;
+                    sum_ww_[idx].second += weight * weight;
+                    sum_xw_[idx].second += value * weight;
+                    sum_xxw_[idx].second += value * value * weight;
+                    if (value < xmin_)
+                        underflow_[idx].second += weight;
+                    else if (value >= xmax_)
+                        overflow_[idx].second += weight;
+                    else
+                    {
+                        histo_[idx][std::floor((value - xmin_) / step_)].second += weight;
+                    }
+                }
+            }
+        }
+
+        /// Write the plot in a ROOT file
+        virtual void Write_TextFormat(std::ostream *output);
+
+        /// Write the plot in a ROOT file
+        //  virtual void Write_RootFormat(std::pair<TH1F*,TH1F*>& histos);
+
+    protected:
+        /// Write the plot in a ROOT file
+        virtual void Write_TextFormatBody(std::ostream *output);
+    };
 
 }
 
