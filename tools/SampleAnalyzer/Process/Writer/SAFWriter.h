cc192ae9a0cc881fbfa825771e80c1e85849da18
Jack Y. Araz <jackaraz@gmail.com>
2023-06-30 14:41:33 +0100

    write weight names

--- tools/SampleAnalyzer/Process/Writer/SAFWriter.h
+++ tools/SampleAnalyzer/Process/Writer/SAFWriter.h
@@ -1,30 +1,28 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  
+//
 //  Copyright (C) 2012-2023 Jack Araz, Eric Conte & Benjamin Fuks
 //  The MadAnalysis development team, email: <ma5team@iphc.cnrs.fr>
-//  
+//
 //  This file is part of MadAnalysis 5.
 //  Official website: <https://github.com/MadAnalysis/madanalysis5>
-//  
+//
 //  MadAnalysis 5 is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  MadAnalysis 5 is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with MadAnalysis 5. If not, see <http://www.gnu.org/licenses/>
-//  
+//
 ////////////////////////////////////////////////////////////////////////////////
-
 
 #ifndef WRITER_SAF_h
 #define WRITER_SAF_h
-
 
 // STL headers
 #include <fstream>
@@ -34,52 +32,44 @@
 // SampleAnalyzer headers
 #include "SampleAnalyzer/Process/Writer/WriterTextBase.h"
 
-
 namespace MA5
 {
 
-class SAFWriter : public WriterTextBase
-{
+    class SAFWriter : public WriterTextBase
+    {
 
-  // -------------------------------------------------------------
-  //                        data members
-  // -------------------------------------------------------------
- protected:
+        // -------------------------------------------------------------
+        //                        data members
+        // -------------------------------------------------------------
+    protected:
+        // -------------------------------------------------------------
+        //                       method members
+        // -------------------------------------------------------------
+    public:
+        /// Constructor without argument
+        SAFWriter() {}
 
+        /// Destructor
+        virtual ~SAFWriter() {}
 
-  // -------------------------------------------------------------
-  //                       method members
-  // -------------------------------------------------------------
- public:
+        /// Read the sample (virtual pure)
+        virtual MAbool WriteHeader(const SampleFormat &mySample);
+        virtual MAbool WriteHeader();
 
-  /// Constructor without argument
-  SAFWriter()
-  { }
+        /// Read the sample (virtual pure)
+        MAbool WriteFiles(const std::vector<SampleFormat> &mySample);
 
-  /// Destructor
-  virtual ~SAFWriter()
-  { }
+        /// Read the event (virtual pure)
+        virtual MAbool WriteEvent(const EventFormat &myEvent,
+                                  const SampleFormat &mySample);
 
-  /// Read the sample (virtual pure)
-  virtual MAbool WriteHeader(const SampleFormat& mySample);
-  virtual MAbool WriteHeader();
+        /// Finalize the event (virtual pure)
+        virtual MAbool WriteFoot(const SampleFormat &mySample);
+        virtual MAbool WriteFoot();
 
-  /// Read the sample (virtual pure)
-  MAbool WriteFiles(const std::vector<SampleFormat>& mySample);
-
-  /// Read the event (virtual pure)
-  virtual MAbool WriteEvent(const EventFormat& myEvent,
-                          const SampleFormat& mySample);
-
-  /// Finalize the event (virtual pure)
-  virtual MAbool WriteFoot(const SampleFormat& mySample);
-  virtual MAbool WriteFoot();
-
-  /// Getting stream
-  std::ostream* GetStream()
-  { return output_; }
- 
-};
+        /// Getting stream
+        std::ostream *GetStream() { return output_; }
+    };
 
 }
 
