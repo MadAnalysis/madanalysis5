409a82bf78e0b4e15ce312b3566889aa8bb339b4
Jack Y. Araz <jackaraz@gmail.com>
2023-11-08 09:31:24 -0500

    cov update for HL scaling

--- madanalysis/misc/run_recast.py
+++ madanalysis/misc/run_recast.py
@@ -22,19 +22,34 @@
 ################################################################################
 
 
-from __future__                                                 import absolute_import
-from madanalysis.install.detector_manager                       import DetectorManager
-from madanalysis.configuration.delphesMA5tune_configuration     import DelphesMA5tuneConfiguration
-from madanalysis.configuration.delphes_configuration            import DelphesConfiguration
-from madanalysis.IOinterface.folder_writer                      import FolderWriter
-from madanalysis.IOinterface.job_writer                         import JobWriter
-from madanalysis.IOinterface.library_writer                     import LibraryWriter
-from madanalysis.misc.histfactory_reader                        import HF_Background, HF_Signal, get_HFID
-from collections                                                import OrderedDict
-from shell_command                                              import ShellCommand
-from string_tools                                               import StringTools
-from six.moves                                                  import map, range, input
-import copy, logging, math, os, shutil, time, sys, json
+from __future__ import absolute_import
+
+import copy
+import json
+import logging
+import math
+import os
+import shutil
+import sys
+import time
+from collections import OrderedDict
+import numpy as np
+
+from shell_command import ShellCommand
+from six.moves import input, map, range
+from string_tools import StringTools
+
+from madanalysis.configuration.delphes_configuration import \
+    DelphesConfiguration
+from madanalysis.configuration.delphesMA5tune_configuration import \
+    DelphesMA5tuneConfiguration
+from madanalysis.install.detector_manager import DetectorManager
+from madanalysis.IOinterface.folder_writer import FolderWriter
+from madanalysis.IOinterface.job_writer import JobWriter
+from madanalysis.IOinterface.library_writer import LibraryWriter
+from madanalysis.misc.histfactory_reader import (
+    HF_Background, HF_Signal,get_HFID
+)
 
 
 class RunRecast():
@@ -1098,14 +1113,6 @@ class RunRecast():
                                     continue
                                 i = item["cov_regions"].index(child.attrib["id"])
                                 j = item["cov_regions"].index(rchild.attrib["region"])
-                                if self.main.recasting.error_extrapolation=='sqrt':
-                                    myval = round(math.sqrt(myval)*lumi_scaling,8)
-                                elif self.main.recasting.error_extrapolation=='linear':
-                                    myval *= lumi_scaling**2
-                                else:
-                                    myval = round(myval*lumi_scaling**2*self.main.recasting.error_extrapolation[0]**2 + \
-                                                  math.sqrt(myval)*lumi_scaling*self.main.recasting.error_extrapolation[1]**2,8)
-
                                 self.cov_config[cov_subset]["covariance"][i][j] = myval
                     else:
                         self.logger.warning('Invalid info file (' + analysis+ '): unknown region subtag.')
@@ -1130,6 +1137,22 @@ class RunRecast():
         tmp = {}
         for cov_subset, item in self.cov_config.items():
             if item["covariance"] != []:
+                cov = np.array(item["covariance"])
+                sigma = np.sqrt(np.diag(cov))
+                invsigma = np.linalg.inv(np.diag(sigma))
+                corr = invsigma @ cov @ invsigma
+                
+                if self.main.recasting.error_extrapolation=='sqrt':
+                    new_sigma = round(math.sqrt(sigma)*lumi_scaling,8)
+                elif self.main.recasting.error_extrapolation=='linear':
+                    new_sigma *= lumi_scaling**2
+                else:
+                    new_sigma = sigma * lumi_scaling**2 * self.main.recasting.error_extrapolation[0]**2 + \
+                        np.sqrt(sigma) * lumi_scaling * self.main.recasting.error_extrapolation[1]**2
+
+                new_sigma_matrix = np.diag(new_sigma)
+                new_cov = new_sigma_matrix @ corr @ new_sigma_matrix
+                item["covariance"] = new_cov.tolist()
                 tmp[cov_subset] = item
         self.cov_config = tmp
 
@@ -1834,8 +1857,8 @@ def pyhf_wrapper(*args, **kwargs):
             return obs values
     """
     import pyhf
+    from numpy import isnan, ndarray, warnings
     from pyhf.optimize import mixins
-    from numpy import warnings, isnan, ndarray
 
     # Scilence pyhf's messages
     pyhf.pdf.log.setLevel(logging.CRITICAL)
@@ -1947,6 +1970,7 @@ def pyhf_wrapper(*args, **kwargs):
 
 def cls(NumObserved, ExpectedBG, BGError, SigHypothesis, NumToyExperiments, **kwargs):
     import scipy.stats
+
     # generate a set of expected-number-of-background-events, one for each toy
     # experiment, distributed according to a Gaussian with the specified mean
     # and uncertainty
