c9d1de88ad2b1ccf7c83e1458ba303992a7e1f60
BFuks <fuks@lpthe.jussieu.fr>
2024-08-30 16:53:07 +0200

    small bug fix, again + versioning

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -37,8 +37,8 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.10.16";
-const std::string Configuration::sampleanalyzer_date_ = "2024/07/31";
+const std::string Configuration::sampleanalyzer_version_ = "1.10.17";
+const std::string Configuration::sampleanalyzer_date_ = "2024/08/30";
 // DO NOT TOUCH THESE LINES
 
 // -----------------------------------------------------------------------------
