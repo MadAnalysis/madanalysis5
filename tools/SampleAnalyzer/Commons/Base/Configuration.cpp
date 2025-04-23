c5451edd47ed2f5328916bb361a1d399b7ddc437
Jack Y. Araz <jackaraz@gmail.com>
2025-04-23 16:56:38 +0100

    update version

--- tools/SampleAnalyzer/Commons/Base/Configuration.cpp
+++ tools/SampleAnalyzer/Commons/Base/Configuration.cpp
@@ -37,8 +37,8 @@ using namespace MA5;
 // Initializing static data members
 // -----------------------------------------------------------------------------
 // DO NOT TOUCH THESE LINES
-const std::string Configuration::sampleanalyzer_version_ = "1.10.17";
-const std::string Configuration::sampleanalyzer_date_ = "2024/08/30";
+const std::string Configuration::sampleanalyzer_version_ = "1.11.0";
+const std::string Configuration::sampleanalyzer_date_ = "2025/04/23";
 // DO NOT TOUCH THESE LINES
 
 // -----------------------------------------------------------------------------
