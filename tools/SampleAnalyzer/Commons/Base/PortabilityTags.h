a551f8fd65e6427559243102fe96d06edfbf227f
BFuks <fuks@lpthe.jussieu.fr>
2026-04-02 07:19:15 +0200

    changing 2012-2025 to 2012-2026

--- tools/SampleAnalyzer/Commons/Base/PortabilityTags.h
+++ tools/SampleAnalyzer/Commons/Base/PortabilityTags.h
@@ -1,45 +1,49 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  
-//  Copyright (C) 2012-2026 Jack Araz, Eric Conte & Benjamin Fuks
+//
+//  Copyright (C) 2012-2016 Eric Conte, Benjamin Fuks
 //  The MadAnalysis development team, email: <ma5team@iphc.cnrs.fr>
-//  
+//
 //  This file is part of MadAnalysis 5.
 //  Official website: <https://github.com/MadAnalysis/madanalysis5>
-//  
+//
 //  MadAnalysis 5 is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  MadAnalysis 5 is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with MadAnalysis 5. If not, see <http://www.gnu.org/licenses/>
-//  
+//
 ////////////////////////////////////////////////////////////////////////////////
 
 
-// SampleHeader headers
-#include "SampleAnalyzer/Commons/DataFormat/EventFormat.h"
-#include "SampleAnalyzer/Commons/DataFormat/SampleFormat.h"
+#ifndef PORTABILITY_CHECK
+#define PORTABILITY_CHECK
 
 
-using namespace MA5;
+// Tags produced automatically by the Portability Check-Up
+// bool    = 1 bytes
+// char    = 1 bytes
+// uchar   = 1 bytes
+// short   = 2 bytes
+// ushort  = 2 bytes
+// int     = 4 bytes
+// uint    = 4 bytes
+// long    = 8 bytes
+// ulong   = 8 bytes
+// llong   = 8 bytes
+// ullong  = 8 bytes
+// float   = 4 bytes
+// double  = 8 bytes
+// ldouble = 8 bytes
 
-// -----------------------------------------------------------------------
-// main program
-// -----------------------------------------------------------------------
-MAint32 main(MAint32 argc, MAchar *argv[])
-{
-  std::cout << "BEGIN-SAMPLEANALYZER-TEST" << std::endl;
-  std::cout << std::endl;
+#define INT_4BYTES  1
+#define LONG_8BYTES 1
 
-  EventFormat event;
-  SampleFormat sample;
 
-  std::cout << "END-SAMPLEANALYZER-TEST" << std::endl;
-  return 0;
-}
+#endif
