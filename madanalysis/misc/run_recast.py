Fix calculation of new_sigma in error extrapolation for recasting

madanalysis/misc/run_recast.py
@@ -1728,7 +1728,7 @@ class RunRecast:
                 corr = invsigma @ cov @ invsigma
 
                 if self.main.recasting.error_extrapolation == "sqrt":
-                    new_sigma = round(math.sqrt(sigma) * lumi_scaling, 8)
+                    new_sigma = np.round(sigma * math.sqrt(lumi_scaling), 8)
                 elif self.main.recasting.error_extrapolation == "linear":
                     new_sigma = sigma * lumi_scaling**2
                 else: